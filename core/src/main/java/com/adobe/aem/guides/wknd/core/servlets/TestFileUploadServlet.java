6e99b055325fcaf72aeb54bbdeed1688cb606d24
Harmeet <you@example.com>
2025-03-18 20:18:56 +0530

    test upload feature

--- core/src/main/java/com/adobe/aem/guides/wknd/core/servlets/TestFileUploadServlet.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/servlets/TestFileUploadServlet.java
@@ -1,12 +1,23 @@
 package com.adobe.aem.guides.wknd.core.servlets;
 import com.adobe.aem.guides.wknd.core.utility.WkndUtility;
 import com.day.cq.commons.Externalizer;
+import com.day.cq.contentsync.handler.util.RequestResponseFactory;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.jackrabbit.api.JackrabbitValueFactory;
 import org.apache.jackrabbit.api.binary.BinaryUpload;
 import org.apache.jackrabbit.commons.JcrUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.request.RequestParameter;
+import org.apache.sling.api.request.builder.Builders;
+import org.apache.sling.api.request.builder.SlingHttpServletRequestBuilder;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
@@ -14,6 +25,7 @@ import org.apache.sling.commons.json.JSONArray;
 import org.apache.sling.commons.json.JSONObject;
 import org.osgi.service.component.annotations.Component;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.net.URI;
 import java.util.Iterator;
@@ -26,6 +38,7 @@ import javax.jcr.Session;
 import javax.jcr.ValueFactory;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Reference;
@@ -37,8 +50,7 @@ import org.slf4j.LoggerFactory;
 @Component(service = { Servlet.class }, property = {
     Constants.SERVICE_DESCRIPTION + "=Test service to validate binary upload service",
     ServletResolverConstants.SLING_SERVLET_METHODS + "=" + HttpConstants.METHOD_POST,
-    ServletResolverConstants.SLING_SERVLET_PATHS + "=/bin/customupload",
-    ServletResolverConstants.SLING_SERVLET_EXTENSIONS + "=txt"
+    ServletResolverConstants.SLING_SERVLET_PATHS + "=/bin/customupload"
 })
 public class TestFileUploadServlet extends SlingAllMethodsServlet{
 
@@ -49,6 +61,7 @@ public class TestFileUploadServlet extends SlingAllMethodsServlet{
 
     @Reference
     private Externalizer externalizer;
+
 
     @Override
     public void doPost(SlingHttpServletRequest request,
@@ -61,7 +74,7 @@ public class TestFileUploadServlet extends SlingAllMethodsServlet{
         // allows to limit number of returned URIs in case the response message size is limited
         // use -1 for unlimited
         final int maxURIs = 50;
-
+                int uploadBinaryStatus = 0;
         final String path = request.getParameter("path");
         final long filesize = Long.parseLong(request.getParameter("filesize"));
 
@@ -81,40 +94,69 @@ public class TestFileUploadServlet extends SlingAllMethodsServlet{
                 Iterable<URI> iter = upload.getUploadURIs();
                 for (URI uri : iter)  {
                     uris.put(uri);
+                    uploadBinaryStatus = uploadBinaryCall(uri, request, resourceResolver);
+                    }
+
+                    json.put("uploadURIs", uris);
+                    
+                    if (uploadBinaryStatus == 200 || uploadBinaryStatus == 201) {
+                        completeUploadProcess(upload.getUploadToken(), path, valueFactory, session);
+                    }else{
+                        LOGGER.info("Error occurred while uploading the file");
+                    }
+    
+                    response.setContentType("application/json");
+                    response.setCharacterEncoding("UTF-8");
+                    response.getWriter().write(json.toString());
                 }
-                json.put("uploadURIs", uris);
-                
-                // provide the client with a complete URL to request later, pass through the path
-                completeUploadProcess(upload.getUploadToken(), path, valueFactory, session);
 
-                response.setContentType("application/json");
-                response.setCharacterEncoding("UTF-8");
-                response.getWriter().write(json.toString());
+                    
+                    
+                }else{
+                    // feature not available, must pass binary via InputStream through vf.createBinary()
+                    LOGGER.info("feature not available, must pass binary via InputStream through vf.createBinary()");
+                }
             }
-
-
-
-            }else{
-                // feature not available, must pass binary via InputStream through vf.createBinary()
-                LOGGER.info("feature not available, must pass binary via InputStream through vf.createBinary()");
-            }
+        }catch(Exception e){
+            LOGGER.info("Error occurred while creating the node " + e.getMessage());
+            response.getWriter().write("Error while upload process" + e.getMessage());
         }
-    }catch(Exception e){
-        LOGGER.info("Error occurred while creating the node " + e.getMessage());
-        response.getWriter().write("node not created");
-    }
-
- }
-
-    private void completeUploadProcess(String uploadToken, String path, JackrabbitValueFactory valueFactory, Session session) throws IllegalArgumentException, RepositoryException {
-        // TODO Auto-generated method stub
-        Binary binary = valueFactory.completeBinaryUpload(uploadToken);
+                    
+        }
+    
+        private int uploadBinaryCall(URI uri, SlingHttpServletRequest request, ResourceResolver resourceResolver) {
+            RequestParameter file = request.getRequestParameter("file");
+            int statuscode = 0;
+            if (Objects.nonNull(file) && StringUtils.isNotBlank(uri.toString())) {
+                try (CloseableHttpClient httpClient =  HttpClientBuilder.create().build()) {
+                    HttpPut httpPut = new HttpPut(uri.toString());
+                    final MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+                    builder.addBinaryBody(file.getName(), file.getInputStream());
+                    final HttpEntity multipart = builder.build();
+                    httpPut.setEntity(multipart);
+                    CloseableHttpResponse httpResponse = httpClient.execute(httpPut);
+                    LOGGER.info("Response code: " + httpResponse.getStatusLine().getStatusCode());
+                    statuscode = httpResponse.getStatusLine().getStatusCode();
+                } catch (IOException e) {
+                   LOGGER.error("Error occurred while uploading the file " + e.getMessage());
+                }         
+            }else{
+                LOGGER.info("File not found");
+            }
+            return statuscode;
+        }
+                    
+       private void completeUploadProcess(String uploadToken, String path, JackrabbitValueFactory valueFactory, Session session) throws IllegalArgumentException, RepositoryException {
+            try{
+                Binary binary = valueFactory.completeBinaryUpload(uploadToken);
 
             Node ntFile = JcrUtils.getOrCreateByPath(path, "nt:file", session);
             Node ntResource = ntFile.addNode("jcr:content", "nt:resource");
             ntResource.setProperty("jcr:data", binary);
             ntResource.setProperty("jcr:mimeType", "image/jpeg");
             session.save();
-        throw new UnsupportedOperationException("Unimplemented method 'completeUploadProcess'");
+            }catch(Exception e){
+                LOGGER.info("Error occurred while completing the last upload process " + e.getMessage());
+            }
     }
 }
